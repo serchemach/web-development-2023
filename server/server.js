ad6bad3534474a82574c2a0931e75f8d0ce8e4d8
serchemach <serchemach@gmail.com>
2023-03-15 09:25:05 +0300

    Add connection to DB

--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
+app.use(express.json())
+const dbConfig = require('./config/db.config')
+
+const db = require('./models')
+
+db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB is connected");
+}).catch((err) => {
+    console.log("Couldn't connect to the DB");
+    console.log(err);
+    process.exit();
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
