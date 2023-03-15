af80e4f2a53d45024b840dabc279a5822bd03c3b
serchemach <serchemach@gmail.com>
2023-03-15 09:24:20 +0300

    Fix typos

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.Promise = gloal.Promise;
+mongoose.Promise = global.Promise;
 
 const db = {};
+db.mongoose = mongoose;
 
-db.user = requre("./user.model");
-db.chat = requre("./chat.model");
-db.message = requre("./message.model");
+db.user = require("./user.model");
+db.chat = require("./chat.model");
+db.message = require("./message.model");
 
 module.exports = db;
